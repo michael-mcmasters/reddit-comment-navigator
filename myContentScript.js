5091c489090e0db4b11af08a40e032324d0a911d
Michael McMasters <jmcmasters411@gmail.com>
2022-12-05 01:04:58 -0500

    Add ability to scroll through top level comments with the keyboard

--- myContentScript.js
+++ myContentScript.js
@@ -2,6 +2,9 @@ const viewEntireDiscussionInfo = { selector: ".j9NixHqtN2j8SKHcdJ0om", innerText
 const sortByButtonInfo = { selector: "#CommentSort--SortPicker", innerText: "Sort By:" }
 const topOptionInfo = { selector: "._10K5i7NW6qcm-UoCtpB3aK", innerText: "top" }
 
+const topCommentSet = new Set();
+const topLevelComments = [];
+let currentIndex = 0;
 
 window.onload = function expandComments() {
     const viewEntireDiscussion = queryElement(viewEntireDiscussionInfo);
@@ -12,6 +15,9 @@ window.onload = function expandComments() {
 
     const topOption = queryElement(topOptionInfo);
     topOption?.click();
+
+    addCommentsAsTheyAppear();
+    handleKeyboardCommentScrolling();
 }
 
 function queryElement(elementInfo) {
@@ -21,4 +27,44 @@ function queryElement(elementInfo) {
             return e;
         }
     }
+}
+
+function addCommentsAsTheyAppear() {
+    const observer = new MutationObserver((mutationList, observer) => {
+        const eles = document.querySelectorAll("._3sf33-9rVAO_v4y0pIW_CH");
+        if (eles.length > 0) {
+            for (const e of eles) {
+                if (e.style.paddingLeft === "16px" && !topCommentSet.has(e.id)) {
+                    topCommentSet.add(e.id);
+                    topLevelComments.push(e);
+                    console.log("Got top level comment")
+                }
+            }
+        }
+    });
+    
+    const config = { attributes: true, childList: true, subtree: true };
+    observer.observe(document.body, config);
+}
+
+function handleKeyboardCommentScrolling() {
+    document.body.addEventListener("keydown", (event) => {
+        console.log(topLevelComments)
+        if (event.key === "1") {
+            console.log("down")
+            --currentIndex;
+        } else if (event.key === "2") {
+            console.log("up")
+            ++currentIndex;
+        }
+
+        console.log(topLevelComments[currentIndex])
+        topLevelComments[currentIndex].focus()
+        topLevelComments[currentIndex].scrollIntoView(true);
+        window.scrollBy(0, -45);
+
+        const innerCommentEle = topLevelComments[currentIndex].querySelectorAll("._3tw__eCCe7j-epNCKGXUKk")[0];
+        innerCommentEle.classList.add("_1vvFtxiq5874iIdCUYlL-d");
+        // Adds border style, but j and keys will still move from another comment.
+    })
 }
