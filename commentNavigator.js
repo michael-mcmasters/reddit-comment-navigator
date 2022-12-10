3bc79c8bb0465a0fe63bf1625439232e6d42b4fb
Michael McMasters <jmcmasters411@gmail.com>
2022-12-10 00:35:39 -0500

    Refactor commentNavigator, don't allow user to scroll out of bounds of comments in map

--- commentNavigator.js
+++ commentNavigator.js
@@ -1,24 +1,28 @@
+"use strict";
+
 class CommentNavigator {
 
     constructor() {
-        this.topCommentSet = new Set();
-        this.topLevelComments = [];
-        this.currentIndex = 0;        
+        this.topLevelCommentsMap = new Map();     
+        this.currentCommentIndex = 0;   
     }
 
     enableNavigation() {
+        // onNewCommentAppears
+        // onKeyboardEnter
         this.addCommentsAsTheyAppear();
         this.handleKeyboardCommentScrolling();
     }
 
     addCommentsAsTheyAppear() {
         const observer = new MutationObserver((mutationList, observer) => {
-            const eles = document.querySelectorAll("._3sf33-9rVAO_v4y0pIW_CH");
-            if (eles.length > 0) {
-                for (const e of eles) {
-                    if (e.style.paddingLeft === "16px" && !this.topCommentSet.has(e.id)) {
-                        this.topCommentSet.add(e.id);
-                        this.topLevelComments.push(e);
+            const elements = document.querySelectorAll("._3sf33-9rVAO_v4y0pIW_CH");
+            if (elements.length > 0) {
+                let index = 0;
+                for (const e of elements) {
+                    if (e.style.paddingLeft === "16px" && !this.topLevelCommentsMap.has(e.id)) {
+                        this.topLevelCommentsMap.set(index, e);
+                        index++;
                         console.log("Got top level comment")
                     }
                 }
@@ -33,24 +37,33 @@ class CommentNavigator {
         document.body.addEventListener("keydown", (event) => {
             if (event.key !== "1" && event.key !== "2")
                 return;
-    
-            console.log(this.topLevelComments)
-            if (event.key === "1") {
-                console.log("down")
-                --this.currentIndex;
-            } else if (event.key === "2") {
-                console.log("up")
-                ++this.currentIndex;
+            
+            switch (event.key) {
+                case "1":
+                    if (this.currentCommentIndex > 0) {
+                        --this.currentCommentIndex
+                    }
+                    break;
+                case "2":
+                    if (this.currentCommentIndex < this.topLevelCommentsMap.size - 1) {
+                        ++this.currentCommentIndex
+                    } else {
+                        alert("This is the last comment");
+                    }
+                    break;
             }
-    
-            console.log(this.topLevelComments[this.currentIndex])
-            this.topLevelComments[this.currentIndex].focus()
-            this.topLevelComments[this.currentIndex].scrollIntoView(true);
-            window.scrollBy(0, -45);
-    
-            const innerCommentEle = this.topLevelComments[this.currentIndex].querySelectorAll("._3tw__eCCe7j-epNCKGXUKk")[0];
-            innerCommentEle.classList.add("_1vvFtxiq5874iIdCUYlL-d");
-            // Adds border style, but j and keys will still move from another comment.
+
+            this.goToComment(this.currentCommentIndex);
         })
+    }
+
+    goToComment(currentCommentIndex) {
+        this.topLevelCommentsMap.get(currentCommentIndex).focus()
+        this.topLevelCommentsMap.get(currentCommentIndex).scrollIntoView(true);
+        window.scrollBy(0, -45);
+
+        const innerCommentEle = this.topLevelCommentsMap.get(currentCommentIndex).querySelectorAll("._3tw__eCCe7j-epNCKGXUKk")[0];
+        // innerCommentEle.classList.add("_1vvFtxiq5874iIdCUYlL-d");
+        // Adds border style, but j and keys will still move from another comment.
     }
 }
